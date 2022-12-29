582a6dc06b8a33e0bb40fe8c19ebe2479891b4b7
GuilhermePCastro <guibkk@gmail.com>
2022-12-29 16:47:16 -0300

    Ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import { View, Text } from "react-native";
 
 const App: React.FC = () => {
     return(
-        <View>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+
             <Text>Meu App de Teste</Text>
         </View>
-    )
-}
+    );
+};
 
-export { App };
+export default App;
