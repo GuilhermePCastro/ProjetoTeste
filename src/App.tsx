6e9dff82ed7c125e535524a52ef48edad21f5f33
GuilhermePCastro <guibkk@gmail.com>
2022-12-29 16:05:26 -0300

    configurações

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const App: React.FC = () => {
+    return(
+        <View>
+            <Text>Meu App de Teste</Text>
+        </View>
+    )
+}
+
+export { App };
